69c599e46993ce5e71de94b6cd03101ce698fc33
erdenebayar dovchindorj <erdenebayr.d@gmail.com>
2020-02-27 19:16:25 +0800

    add nodejs

--- back/nodejs/app.js
+++ back/nodejs/app.js
@@ -1,5 +1,35 @@
 const recorder = require("node-record-lpcm16");
 
+async function translateTextToJapanese(text) {
+  const projectId = "videoapiservice-265212";
+  const location = "global";
+
+  // Imports the Google Cloud Translation library
+  const { TranslationServiceClient } = require("@google-cloud/translate");
+
+  // Instantiates a client
+  const translationClient = new TranslationServiceClient();
+  // Construct request
+  const request = {
+    parent: `projects/${projectId}/locations/${location}`,
+    contents: [text],
+    mimeType: "text/plain", // mime types: text/plain, text/html
+    sourceLanguageCode: "en",
+    targetLanguageCode: "ja"
+  };
+
+  try {
+    // Run request
+    const [response] = await translationClient.translateText(request);
+    let results = [];
+    for (const translation of response.translations) {
+      results.push(translation.translatedText);
+    }
+    return results;
+  } catch (error) {
+    console.error(error.details);
+  }
+}
 // Imports the Google Cloud client library
 const speech = require("@google-cloud/speech");
 
@@ -26,13 +56,15 @@ const request = {
 const recognizeStream = client
   .streamingRecognize(request)
   .on("error", console.error)
-  .on("data", data =>
-    process.stdout.write(
-      data.results[0] && data.results[0].alternatives[0]
-        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-        : `\n\nReached transcription time limit, press Ctrl+C\n`
-    )
-  );
+  .on("data", async data => {
+    if (data.results[0] && data.results[0].alternatives[0]) {
+      const englishText = data.results[0].alternatives[0].transcript;
+      const japaneseText = await translateTextToJapanese(englishText);
+      console.log(englishText + "\n" + japaneseText + "\n");
+    } else {
+      console.log(`\n\nReached transcription time limit, press Ctrl+C\n`);
+    }
+  });
 
 // Start recording and send the microphone input to the Speech API.
 // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
